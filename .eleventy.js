d0ddb490c1a796ddc563075635b4408aa31c5b83
Roman Horokhovatskyy <h0r0m4n@icloud.com>
2020-11-18 22:13:48 +0000

    Templates and head

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ const global = require('./_data/global');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('static');
+    eleventyConfig.addPassthroughCopy('./*.{png,svg,ico}');
+    eleventyConfig.addPassthroughCopy('site.webmanifest');
+    eleventyConfig.addPassthroughCopy('CNAME');
 }
--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@
 
 1. YouTube custom tag: https://raw.githubusercontent.com/chybie/jec-11ty-starter/master/src/blog/2020-05-20-post-one.md
 2. Enviroment variables: https://www.11ty.dev/docs/data-js/
+3. Default layout: https://github.com/11ty/eleventy/issues/380#issuecomment-703002785
