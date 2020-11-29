c8f7ce765ec222c00bd8ae565665dc0476a9f318
Roman Horokhovatskyy <h0r0m4n@icloud.com>
2020-11-29 15:28:34 +0100

    Improved tags

--- .eleventy.js
+++ .eleventy.js
@@ -31,23 +31,23 @@ module.exports = function (eleventyConfig) {
     });
 
     // Image short-code
-    // Usage: {% image "my-image.jpg" "full" "My alt…" "My caption…" %}
+    // Usage: {% image "my-image" "full" "My alt…" "My caption…" %}
     eleventyConfig.addShortcode('image', function(src, full, alt, caption) {
         return outdent`
             <figure ${full ? `class="full"` : ``}>
-                <img src="${src}" ${alt ? `alt="${alt}"` : ``} loading="lazy">
+                <img src="/static/${src}.jpg" srcset="/static/${src}@1.5x.jpg 1.5x, /static/${src}@2x.jpg 2x" ${alt ? `alt="${alt}"` : ``} loading="lazy">
                 ${caption ? `<figcaption>${caption}</figcaption>` : ``}
             </figure>
         `;
     });
 
     // Video short-code
-    // Usage: {% video "my-video.mp4" "full" "My caption…" %}
+    // Usage: {% video "my-video" "full" "My caption…" %}
     eleventyConfig.addShortcode('video', function(src, full, caption) {
         return outdent`
             <figure ${full ? `class="full"` : ``}>
                 <video width="960" height="540" autoplay loop muted playsinline disablePictureInPicture>
-                    <source src="${src}" type="video/mp4">
+                    <source src="/static/${src}.mp4" type="video/mp4">
                 </video>
                 ${caption ? `<figcaption>${caption}</figcaption>` : ``}
             </figure>
--- work/work-four.md
+++ work/work-four.md
@@ -9,17 +9,27 @@ layout: work
 
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ipsam. Deleniti, animi a. Modi, eum similique. Quisquam ipsam fugit at distinctio, repudiandae asperiores officiis quis vero beatae laborum quasi minus.
 
-Test 1 (Image with everything):
-{% image "//placehold.it/300" "Full" "My alt…" "My caption…" %}
+Image (with everything):
+{% image "cefla-euroshop-2020-1" "Full" "My alt…" "My caption…" %}
 
-Test 2 (Image with src and alt):
-{% image "//placehold.it/300" "" "My alt…" "" %}
+Image (with src and alt):
+{% image "cefla-euroshop-2020-2" "" "My alt…" "" %}
 
-Test 3 (Image with only src):
-{% image "//placehold.it/300" "" "" "" %}
+Image (with only src):
+{% image "cefla-euroshop-2020-3" "" "" "" %}
+
+***
 
 Stats (with everything):
 {% stats "Label 1" "Value 1" "Label 2" "Value 2" "Label 3" "Value 3" "Label 4" "Value 4" %}
 
 Stats (with label/value 1):
 {% stats "Label 1" "Value 1" "" "" "" "" "" "" %}
+
+***
+
+Video (with everything):
+{% video "cefla-euroshop-2020-1" "full" "My caption…" %}
+
+Video (with video only):
+{% video "cefla-euroshop-2020-1" "" "" %}
