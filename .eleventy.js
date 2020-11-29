84b0c30f5760c60af5342b56a917fbf9281caac3
Roman Horokhovatskyy <h0r0m4n@icloud.com>
2020-11-29 13:26:19 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -13,11 +13,11 @@ module.exports = function (eleventyConfig) {
         return collectionApi.getFilteredByGlob('work/*.md').reverse();
     });
 
+    // Exclude items with permalink set to false
     function permalinkNotFalse(items) {
         return items.filter(item => {
             return (item.data.permalink !== false)
         })
     }
-
     eleventyConfig.addFilter('permalinkNotFalse', permalinkNotFalse);
 }
