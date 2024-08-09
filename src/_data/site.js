e05746b3750a9e42be0557b83cf6d9e60210f99d
Roman Horokhovatskyy <h0r0m4n@icloud.com>
2024-08-09 13:11:28 +0100

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,14 @@
 module.exports = {
     title: 'Roman Horokhovatskyy',
     description: 'Personal homepage of Roman Horokhovatskyy a Ukrainian multi-disciplinary designer based in Dublin, Ireland.',
-    keywords: 'Roman, Horokhovatskyy, @horoman, @h0r0m4n, Product Design, Interaction Design, User Interface Design, User Experience Design, Design Systems',
+    keywords: 'Roman, Horokhovatskyy, @horoman, @h0r0m4n, Product Design, Design Strategy, Interaction Design, User Interface Design, User Experience Design, Design Systems',
     author: 'Roman Horokhovatskyy',
     location: 'Dublin, Ireland',
     nickname: 'h0r0m4n',
     mail: 'me@horoman.com',
     starting: 2008,
     password: 376301,
-    version: '5.6',
+    version: '5.7',
     environment: process.env.ELEVENTY_ENV,
     url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://horoman.com'
 };
